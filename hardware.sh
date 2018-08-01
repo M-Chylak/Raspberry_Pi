5efc3c9a7e22440b167f7da974d45c165a60baeb
M-Chylak <40693380+M-Chylak@users.noreply.github.com>
2018-08-01 10:35:57 +0200

    hardware

---
+++ hardware.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+Hardware information:
+
+lshw
+
+lscpu
+
+lsblk
+
+lsusb
+
+lspci -t
+
+lsscsi
+
+hdparm /dev/sda1
+
+fdisk -l
+
+dmidecomde -t memory
+
+dmidecode -t system
+
+dmidecode -t bios
+
+dmidecode -t processor
